51a81bedde8ba19c9a249a7d439c796752ab74c3
Isaac Spanier <ispanier@iastate.edu>
2020-07-11 20:25:19 -0500

    added both exercise and user models and routes

---
+++ server/models/exercise.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username: {type: String, required: true},
+    description: {type: String, required: true},
+    duration: {type: String, required: true},
+    date: {type: String, required: true},
+},{
+    timestamps: true,
+});
+
+const Excerise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Excerise;
